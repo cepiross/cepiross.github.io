6242e8e209e4fc8ef3bceff9801a1f45e49ed433
Jinhang Choi <cepiross@gmail.com>
2022-02-26 17:55:17 -0800

    Use citation-js to parse bibtex, then pass it to pug

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -4,16 +4,36 @@ const upath = require('upath');
 const pug = require('pug');
 const sh = require('shelljs');
 const prettier = require('prettier');
+const cite = require('citation-js');
 
 module.exports = function renderPug(filePath) {
     const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
     const srcPath = upath.resolve(upath.dirname(__filename), '../src');
 
+    let pubList = {};
+    const bibPath = upath.resolve(srcPath, 'assets/bib');
+    sh.ls(bibPath.concat('/*.bib')).forEach(function (file) {
+        let rdObj = fs.readFileSync(file);
+        let bibObj = new cite(rdObj.toString());
+        let bibId = bibObj.data[0].id
+        let bibText = bibObj.format('bibliography', {
+            format: 'text',
+            template: 'apa'
+        });
+        bibText = bibText.split('https://')
+        pubList[bibId] = {};
+        pubList[bibId]['bib'] = bibText[0].trim()
+        if (bibText.length > 1) {
+            pubList[bibId]['doi'] = 'https://' + bibText[1].trim()
+        }
+    });
+
     console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
     const html = pug.renderFile(filePath, {
         doctype: 'html',
         filename: filePath,
-        basedir: srcPath
+        basedir: srcPath,
+        pub: pubList
     });
 
     const destPathDirname = upath.dirname(destPath);
